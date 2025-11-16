d84c33871ed3caa18ea2f9b26d1a644fa14e09b1
Alaa Khalil <ak2139060@gmail.com>
2025-11-16 23:17:59 +0100

    feat: add ProfileService deleteAccount method
    - Add deleteAccount method for soft delete functionality
    - Update updateProfile to handle civil_status and family_status
    - Return boolean for delete operation success

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -24,6 +24,8 @@ class ProfileService
             'last_name'  => $data['last_name'] ?? $user->last_name,
             'email'      => $data['email'] ?? $user->email,
             'phone'      => $data['phone'] ?? $user->phone,
+            'civil_status' => $data['civil_status'] ?? $user->civil_status,
+            'family_status' => $data['family_status'] ?? $user->family_status,
         ]);
     }
 
@@ -31,5 +33,10 @@ class ProfileService
     {
         $user->password = Hash::make($newPassword);
         $user->save();
+    }
+
+    public function deleteAccount(User $user): bool
+    {
+        return $user->delete();
     }
 }
