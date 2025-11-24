eb3da3dc31bf64c9ecb212fc5238b56575a25249
Alaa Khalil <ak2139060@gmail.com>
2025-11-24 18:51:25 +0100

    db: add blog factories for seeding (Blog, BlogComment, BlogLike, BlogBookmark)

---
+++ database/factories/BlogBookmarkFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BlogBookmark;
+use App\Models\Blog;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BlogBookmark>
+ */
+class BlogBookmarkFactory extends Factory
+{
+    protected $model = BlogBookmark::class;
+
+    public function definition(): array
+    {
+        return [
+            'blog_id' => Blog::inRandomOrder()->value('id') ?? Blog::factory(),
+            'user_id' => User::inRandomOrder()->value('id') ?? User::factory(),
+        ];
+    }
+}
