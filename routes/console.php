7b75e3b8655794875cc03e016b44607cd236c39d
Alaa Khalil <ak2139060@gmail.com>
2026-02-17 01:57:50 +0100

    chore: register newsletter dispatch command in routes/console and remove Kernel (bootstrap handles commands)

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,17 @@ Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Dispatch scheduled newsletters (useable from cron or scheduler)
+Artisan::command('newsletter:dispatch-scheduled', function () {
+    $service = app(\App\Services\NewsletterCampaignService::class);
+
+    $campaigns = $service->getScheduled();
+
+    foreach ($campaigns as $campaign) {
+        if ($campaign->scheduled_at && $campaign->scheduled_at->lte(now())) {
+            $service->sendNow($campaign->id);
+            $this->info('Dispatched campaign '.$campaign->id);
+        }
+    }
+})->describe('Dispatch scheduled newsletter campaigns');
